d9d90f1232f8f32b1f13f9908a10660baa6f1c5d
SolovyevAlexey <20032013alex@gmail.com>
2023-04-29 15:32:05 +0300

    reworked base handler

--- src/application.py
+++ src/application.py
@@ -1,12 +1,12 @@
 from telegram.ext import Application, CommandHandler
 
-from bot.core.settings import Settings
+from bot.core.settings import settings
 from bot.conversations.main_application import start
 
 
 def main() -> None:
     """Initialize a Telegram bot application with a main handler."""
-    application = Application.builder().token(Settings.telegram_token).build()
+    application = Application.builder().token(settings.telegram_token).build()
     application.add_handler(CommandHandler("start", start))
     application.run_polling()
 
