9677cd563c5d2b4756e1ac5053562fbfd1de960b
SolovyevAlexey <20032013alex@gmail.com>
2023-05-08 17:36:02 +0300

    main_handler

--- src/application.py
+++ src/application.py
@@ -1,13 +1,29 @@
-from telegram.ext import Application, CommandHandler
+from telegram.ext import (Application, CommandHandler,
+                          ConversationHandler, CallbackQueryHandler)
 
 from bot.core.settings import settings
-from bot.conversations.main_application import start
+from bot.conversations.main_application import start, MAIN_MENU, main_menu
 
 
 def main() -> None:
     """Initialize a Telegram bot application with a main handler."""
     application = Application.builder().token(settings.telegram_token).build()
-    application.add_handler(CommandHandler("start", start))
+    main_handler = ConversationHandler(
+        entry_points=[
+            CommandHandler('start', start),
+            CallbackQueryHandler(main_menu, pattern=None)
+        ],
+        states={
+            MAIN_MENU: [
+                ...
+            ],
+        },
+        fallbacks=[
+            CommandHandler('start', start)
+        ],
+        allow_reentry=True,
+    )
+    application.add_handler(main_handler)
     application.run_polling()
 
 
