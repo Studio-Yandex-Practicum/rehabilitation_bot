b9fcfd67f8e1539d7136b0ea5a4ec0e578e8d8ef
SolovyevAlexey <102797057+SolovyevAlexey@users.noreply.github.com>
2023-05-14 16:00:59 +0300

    Update src/bot/handlers.py
    Co-authored-by: Yuliy Mitryashkin <58391311+JM1k1@users.noreply.github.com>

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,23 +1,23 @@
-from telegram.ext import (CommandHandler,
-                          ConversationHandler, CallbackQueryHandler)
+from telegram.ext import (CallbackQueryHandler, CommandHandler,
+                          ConversationHandler)
 
 from bot.constants.state import MAIN_MENU
-from bot.conversations.main_application import start, main_menu
-
+from bot.conversations.main_application import main_menu, start
 
 main_handler = ConversationHandler(
-        entry_points=[
-            CommandHandler('start', start),
-        ],
-        states={
-            MAIN_MENU: [
+    entry_points=[
+        CommandHandler('start', start),
+    ],
+    states={
+        MAIN_MENU: [
             CallbackQueryHandler(
-                menu_application.menu, pattern=fr"^{key.MENU}_\S*$"
+                # menu_application.menu, pattern=fr"^{key.MENU}_\S*$"
             ),
-            ],
-        },
-        fallbacks=[
-            CommandHandler('menu', main_application.main_menu)
         ],
-        allow_reentry=True,
-    )
+    },
+    fallbacks=[
+        CommandHandler('menu', main_menu)
+    ],
+    allow_reentry=True,
+)
+
