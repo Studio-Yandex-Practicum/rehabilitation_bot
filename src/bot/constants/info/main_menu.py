1d26755794a087396c666c639bbdd47000a548fc
Никита Нестеренко <i@e-noth.ru>
2023-05-17 00:46:59 +0300

    убрал models и перенес regex

---
+++ src/bot/constants/info/main_menu.py
@@ -0,0 +1,29 @@
+from . import key
+
+
+MAIN_MENU = {
+    f"{key.MENU}_MAIN": {
+        key.NAME: "Главное меню Кнопка",
+        key.DESCRIPTION: "Чтобы вступить в сообщество необходимо заполнить краткую анкету.",
+        key.MENU: [f"{key.MENU}_SUB",]
+    }
+}
+
+SUBMENU = {
+    f"{key.MENU}_SUB": {
+        key.NAME: "Кнопка 1",
+        key.DESCRIPTION: "Описание 1",
+        key.PARENT: f"{key.MENU}_MAIN",
+        key.MENU: [f"{key.MENU}_SUB2",]
+    },
+    f"{key.MENU}_SUB2": {
+        key.NAME: "Кнопка 2",
+        key.DESCRIPTION: "Описание 2",
+        key.PARENT: f"{key.MENU}_SUB"
+    }
+}
+
+ALL_MENU = {
+    **MAIN_MENU,
+    **SUBMENU,
+}
