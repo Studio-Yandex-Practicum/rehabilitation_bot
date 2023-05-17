04a7d5d366cc6ccd76d8ad3a37e16d3793ea01c9
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-05-17 16:42:47 +0300

    Добавил регулярку в src.bot.core.db

--- src/bot/core/db/schemas.py
+++ src/bot/core/db/schemas.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel, Field
 
+from bot.constants.info.text import FULL_NAME_REGEX
 from src.bot.constants.info.text import REGEX_NON_LATIN
 
 
 class FormBase(BaseModel):
-    full_name: str = Field(None, regex=REGEX_NON_LATIN)
+    full_name: str = Field(None, regex=FULL_NAME_REGEX)
     speciality: str = Field(None, regex=REGEX_NON_LATIN)
     job: str = Field(None, regex=REGEX_NON_LATIN)
     experience: str = Field(None, regex=REGEX_NON_LATIN)
