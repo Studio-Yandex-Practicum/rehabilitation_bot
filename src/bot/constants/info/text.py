2331371ba5ed34391cbfecd4bece6c3ae68b6e44
Yana-Denisova <ladymacbeth@list.ru>
2023-05-29 12:04:47 +0300

    Add comments

--- src/bot/constants/info/text.py
+++ src/bot/constants/info/text.py
@@ -1,4 +1,9 @@
+# MAIN MENU
 START_MESSAGE = 'Здесь должно быть приветственное сообщение (Заглушка)!'
+
+STOP_MESSAGE = 'Работа приложения остановлена.'
+
+
+# REGEXP
 REGEX_NON_LATIN = r"^[^a-zA-Z]*$"
 REGEX_FULL_NAME = r"^([А-Яа-яЁё\-]+\b {,3}){2,}$"
-STOP_MESSAGE = 'Работа приложения остановлена.'
