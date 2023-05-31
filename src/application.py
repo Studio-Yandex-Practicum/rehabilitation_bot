f5bf4c5107f2d97b83f497d6ae9c80cd18329dd2
Александр Чулков <monstrmix692@yandex.ru>
2023-05-31 15:26:00 +0300

    добавлена нейронка нужна доработка

--- src/application.py
+++ src/application.py
@@ -1,14 +1,19 @@
 from telegram.ext import Application
 
 from bot.core.settings import settings
-from bot.handlers import main_handler, moderation_handler
+from bot.handlers import (
+    main_handler,
+    moderation_handler,
+    moderation_spam_handler,
+)
 
 
 def main() -> None:
     """Initialize a Telegram bot application with a main handler."""
     application = Application.builder().token(settings.telegram_token).build()
     application.add_handler(main_handler)
-    application.add_handler(moderation_handler)
+    application.add_handler(moderation_handler, group=0)
+    application.add_handler(moderation_spam_handler, group=1)
     application.run_polling()
 
 
--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,10 +1,17 @@
 
-from telegram.ext import (CommandHandler, ConversationHandler, MessageHandler,
-                          filters)
+from telegram.ext import (
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
 from bot.constants.state import MAIN_MENU
 from bot.conversations.main_application import main_menu, start, stop
-from bot.conversations.moderation_application import chat_moderation
+from bot.conversations.moderation_application import (
+    chat_moderation,
+    chat_moderation_spam,
+)
 
 
 main_handler = ConversationHandler(
@@ -30,4 +37,9 @@ main_handler = ConversationHandler(
 moderation_handler = MessageHandler(
     filters.TEXT,
     chat_moderation
+)
+
+moderation_spam_handler = MessageHandler(
+    filters.TEXT,
+    chat_moderation_spam
 )
