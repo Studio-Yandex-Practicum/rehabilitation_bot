f2d297aa181f6adf6430725140cdb6e203717f73
Артём Куркин <artykn88@gmail.com>
2023-06-16 10:21:42 +0300

    imports are configured

--- src/bot/core/logger.py
+++ src/bot/core/logger.py
@@ -1,7 +1,7 @@
 import logging.config
 from pathlib import Path
 
-from src.bot.core.settings import settings
+from bot.core.settings import settings
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
