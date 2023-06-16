70c5a9bc2055cf41967c79b4fb467eba16959bcf
Артём Куркин <artykn88@gmail.com>
2023-06-16 10:11:30 +0300

    added examples of constants

--- src/bot/constants/info/menu.py
+++ src/bot/constants/info/menu.py
@@ -1,4 +1,4 @@
-from src.bot.constants import key
+from bot.constants import key
 
 
 MAIN_MENU = {
@@ -17,25 +17,13 @@ SUBMENU = {
         key.NAME: "Кнопка 1",
         key.DESCRIPTION: "Описание 1",
         key.PARENT: f"{key.MENU}_MAIN",
-        key.MENU: [
-            f"{key.MENU}_SUB2",
-        ],
     },
     f"{key.MENU}_SUB2": {
         key.NAME: "Кнопка 2",
         key.DESCRIPTION: "Описание 2",
         key.PARENT: f"{key.MENU}_MAIN",
-        key.MENU: [
-            f"{key.MENU}_SUB3",
-        ],
     },
-    f"{key.MENU}_SUB3": {
-        key.NAME: "Назад",
-        key.DESCRIPTION: "Описание 3",
-        key.PARENT: f"{key.MENU}_MAIN",
-    }
 }
-
 
 ALL_MENU = {
     **MAIN_MENU,
